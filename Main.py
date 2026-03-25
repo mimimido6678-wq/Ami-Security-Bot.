2d7b00ca837bc53972c5df6698ff1903b47556d1
mimimido6678-wq <mimi.mido6678@gmail.com>
2026-03-25 01:12:31 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,2 +1,16 @@
-pyTelegramBotAPI
-flask
+import telebot
+
+# حط التوكن ديالك هنا
+API_KEY = "TOKEN_HNA"
+
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "مرحباً! أنا بوت الحماية الخاص بك.")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
